97fb2220f3f119e2157ff463cddc2301cc90e99e
kwellfare <30420803+kwellfare@users.noreply.github.com>
2017-11-16 14:34:17 -0600

    Track user votes
    Updated to insert the current users id into the database. This allows restrictions to the database

--- methods/methods.js
+++ methods/methods.js
@@ -7,28 +7,31 @@ import { ReactiveVar } from 'meteor/reactive-var';
 
 Meteor.methods({
 
-	addVote: function(pollID, voteID, voters) {
+	addVote: function(pollID, voteID) {
 
-	//finds current user
-	var userId = Meteor.userId(); 
-   // console.log('The current user is ' + '' + userId);
+		//finds current user
+		var userId = Meteor.userId(); 
+	        //console.log('The current user is ' + '' + userId);
        
 		//create the incrementing object so that it can be added to the corresponding vote
 		var voteString = 'choices.' + voteID + '.votes';
-		console.log('The objects to be updated ' + ''+ voteString);
+		//var voted = 'voters.' + pollID + '.username';
+		//console.log('The objects to be updated ' + ''+ voteString);
+		//console.log('The objects to be added ' + ''+ voted);
+
 		var actions ={};
 		actions[voteString] = 1;
-
+       
         
-		//increment the number of votes for this choice
-		Polls.update(
-		{ _id: pollID },
-		{ $inc: actions }, //MonoDB's $inc operator adds 1 to the vote
-	    { $addToSet: {voters:userId } } //records username of voter
+		    //increment the number of votes for this choice
+		    Polls.update({ _id: pollID}, {$inc: actions }); 
+		    //MonoDB's $inc operator adds 1 to the vote
+		    Polls.update ({_id: pollID}, {$addToSet: { voters: userId}});
+		    //records username of voter
 
-		);
-//console.log('the poll id updated was ' + '' + pollID);
-		//console.log('the voters are' + voters);
+		
+	//console.log('the poll id updated was ' + '' + pollID);
+	//console.log(Polls.find().fetch());
 
 	}
 
